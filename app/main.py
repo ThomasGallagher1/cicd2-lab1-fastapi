9ee582e78b798daae5316395e7f6757fb53e7f55
ThomasGallagher1 <118356796+ThomasGallagher1@users.noreply.github.com>
2026-09-24 14:31:01 +0000

    add user read endpoints

--- app/main.py
+++ app/main.py
@@ -24,4 +24,18 @@ def add_user(new_user: UserCreate):
 def get_users():
     return users
 
-#Skipping to the PR
+@app.get("/api/users") 
+def get_users(): 
+    return users 
+ 
+ 
+@app.get("/api/users/{user_id}") 
+def get_user(user_id: int): 
+    for existing_user in users: 
+        if existing_user.user_id == user_id: 
+            return existing_user 
+ 
+    raise HTTPException( 
+        status_code=status.HTTP_404_NOT_FOUND, 
+        detail="User not found", 
+    ) 
